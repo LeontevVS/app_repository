680165e9087e752123cd16dcafb51695bb14b41c
vitaliy <vitalij.leontjev@tetrika.school>
2024-03-18 12:50:35 +0300

    some_fixes

--- src/api/v1/__init__.py
+++ src/api/v1/__init__.py
@@ -2,8 +2,10 @@ from fastapi import APIRouter
 from . import (
     auth,
     users,
+    test_handlers,
 )
 
 router = APIRouter(prefix='/api')
 router.include_router(auth.router)
 router.include_router(users.router)
+router.include_router(test_handlers.router)
