a366f62304a163eada7797b55160dec93b5afefe
vitaliy <vitalij.leontjev@tetrika.school>
2024-12-28 21:06:38 +0300

    allowed_all_cors

--- shop/app/app.py
+++ shop/app/app.py
@@ -1,10 +1,20 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from modules.users.module import get_module_router
 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(get_module_router())
 uvicorn.run(
     app=app,
